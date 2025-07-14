88ca19ae56b31770dc79ecd7b0e974238c80dec0
SUBHAJIT GHOSH <subhajitghosh@SUBHAJITs-Mac-mini.local>
2025-07-14 18:35:15 +0530

    first commit

--- android/app/src/main/java/com/astrovigyaastrologer/MainApplication.kt
+++ android/app/src/main/java/com/astrovigyaastrologer/MainApplication.kt
@@ -1,4 +1,4 @@
-package com.travel.travelapp
+package com.astrovigyaastrologer
 
 import android.app.Application
 import com.facebook.react.PackageList
--- ios/AstrovigyaAstrologer/AppDelegate.swift
+++ ios/AstrovigyaAstrologer/AppDelegate.swift
@@ -24,7 +24,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     window = UIWindow(frame: UIScreen.main.bounds)
 
     factory.startReactNative(
-      withModuleName: "Group Travel",
+      withModuleName: "Astrovigya - Astrologer",
       in: window,
       launchOptions: launchOptions
     )
